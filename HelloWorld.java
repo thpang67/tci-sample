a5eb0cd6a6a5c45bb8f2871a883e0c6e924f4955
Thomas S. Pangborn <thomas.s.pangborn@gmail.com>
2015-06-09 18:28:58 -0400

    Create HelloWorld.java
    First application to be built by Travis-CI

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * The HelloWorld class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+
+        //
+        // Print : Hello World!
+        //
+        System.out.println("Hello World!"); 
+        
+    }
+}
